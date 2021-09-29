11619cf71fa5c62b3f080d724bd93d514ebf01f0
cristobale2000 <cristobalescamilla2000@gmail.com>
2021-09-29 02:19:43 -0500

    LCS y Palíndromos

--- act.cpp
+++ act.cpp
@@ -9,7 +9,6 @@
  * 29 de septiembre del 2021
  */
 
-#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -51,40 +50,102 @@ vector<int> PMP(string texto, string patron, int &cont){
     return sal;
 }
 
-int LCS(int mat[1000][1000], string s1, string s2){
-    int max;
-    for (int i = 0; i < 1000; i++) {
-        if (s1[i] == s2[0]) {
+// Complejidad: O(n)
+string manacher(string texto, int &inicio){
+	string T = "";
+	int n=texto.length();
+	for (int i=0; i<n; i++){
+		T += "|";
+		T += texto[i];
+	}
+	T += "|";
+	n = T.length();
+	vector<int> L(n);
+	L[0] = 0;
+	L[1] = 1;
+	int maxLong=0, maxCentro=0;
+	bool exp;
+	for (int c=1, li=0, ri=2; ri<n; ri++){
+		li = c-(ri-c);
+		exp = false;
+		if (c-maxLong <= ri && ri >= c+maxLong){
+			if (L[li] < (c+L[c]-ri)){
+				L[ri] = L[li];
+			}
+			else if(L[li] == (c + L[c]) - ri && (c + L[c]) == 2*n){
+				L[ri] = L[li];
+			}
+			else if(L[li] == (c + L[c]) - ri && (c + L[c]) < 2*n){
+				L[ri] = L[li];
+				exp = true;
+			}
+			else if(L[li] > (c + L[c]) - ri && (c + L[c]) < 2*n){
+				L[ri] = (c+L[c]) - ri;
+				exp = true;
+			}
+		}
+		else{
+			L[ri] = 0;
+			exp = true;
+		}
+		if (exp){
+			while((ri + L[ri] < n) && (ri - L[ri] > 0) && (T[ri - L[ri] - 1] == T[ri + L[ri] + 1])){
+				L[ri]++;
+			}
+		}
+		c = ri;
+		if (L[ri] > maxLong){
+	    	maxLong = L[ri];
+	    	maxCentro = ri;
+	    }
+	}
+	inicio = (maxCentro-maxLong)/2;
+	string salida = "";
+	for (int i=inicio; i<(inicio+maxLong); i++){
+		salida += texto[i];
+	}
+	
+	return salida;
+}
+
+string lcs(int mat[1000][1000], string s1, string s2){
+    int len1 = s1.length() >= 1000 ? 1000 : s1.length(), len2 = s2.length() >= 1000 ? 1000 : s2.length(), maxLen = 0, indiceFinal = -1;
+
+    for(int i = 0; i < len1; i++){
+        if(s1[i] == s2[0]){
             mat[i][0] = 1;
-            max = 1;
+            maxLen = 1;
         }
-        else {
+        else
             mat[i][0] = 0;
-        }
     }
-    for (int j = 0; j < 1000; j++) {
-        if (s1[0] == s2[j]) {
+
+    for(int j = 1; j < len2; j++){
+        if(s2[j] == s1[0]){
             mat[0][j] = 1;
-            max = 1;
+            maxLen = 1;
         }
-        else {
+        else
             mat[0][j] = 0;
-        }
     }
-    for (int i = 1; i < 1000 - 1; i++) {
-        for (int j = 1; j < 1000 - 1; j++) {
-            if (s1[i] == s2[j]) {
-                mat[i][j] = mat[i - 1][j - 1] + 1;
-                if (mat[i][j] > max) {
-                    max = mat[i][j];
+
+    for(int i = 1; i < len1; i++){
+        for(int j = 1; j < len2; j++){
+            if(s1[i] == s2[j]){
+                mat[i][j] = mat[i-1][j-1] + 1;
+                if(mat[i][j] > maxLen){
+                    maxLen = mat[i][j];
+                    indiceFinal = i+1;
                 }
             }
-            else {
+            else
                 mat[i][j] = 0;
-            }
         }
+
     }
-    return max;
+
+
+    return maxLen == 0 ? "No hay substrings en común." : s1.substr(indiceFinal - maxLen, maxLen);
 }
 
 void readData(vector<string> &mcodes, vector<string> &transmissions){
@@ -122,6 +183,7 @@ int main(){
     int cont;
     vector<int> vect;
 
+    check << DIVISOR2 << endl;
     for(int i = 0; i < mcodes.size(); i++){
         check << "Código: " << mcodes[i] << endl;
         for(int j = 0; j <= 2; j++){
@@ -138,22 +200,35 @@ int main(){
             }
         }
 
-        if(i < mcodes.size()-1)
-            check << DIVISOR1 << endl;
-        else
-            check << DIVISOR2 << endl;
+        check << (i < mcodes.size()-1 ? DIVISOR1 : DIVISOR2) << endl;
     }
 
+    // Impresión de datos: Palíndromo más grande
+    string palindromo;
+    int posicion = 0;
+    check << "Palíndromo más grande:" << endl;
+    for(int i = 0; i <= 2; i++){
+        palindromo = manacher(palindromo, posicion);
+        check << "transmission" << i+1 << ".txt ==> ";
+        
+        if(palindromo != ""){
+            check << "Posición: " << posicion << endl;
+            check << palindromo << endl;
+        }
+        else
+            check << "No hay palíndromos." << endl;
+
+        check << (i < 2 ? DIVISOR1 : DIVISOR2) << endl;
+    }
 
     // Impresión de datos: Substrings más largos
     int mat[1000][1000];
-    check << "Substrings más largos:" << endl;
-    if (LCS(mat, transmissions[0], transmissions[1]))
-    check << "transmission1.txt & transmission2.txt ==> " << LCS(mat, transmissions[0], transmissions[1]) << endl;
+    check << "Substrings en común más largos:" << endl;
+    check << "transmission1.txt & transmission2.txt ==> " << lcs(mat, transmissions[0], transmissions[1]) << endl;
     check << DIVISOR1 << endl;
-    check << "transmission1.txt & transmission3.txt ==> " << LCS(mat, transmissions[0], transmissions[2]) << endl;
+    check << "transmission1.txt & transmission3.txt ==> " << lcs(mat, transmissions[0], transmissions[2]) << endl;
     check << DIVISOR1 << endl;
-    check << "transmission2.txt & transmission3.txt ==> " << LCS(mat, transmissions[1], transmissions[2]) << endl;
+    check << "transmission2.txt & transmission3.txt ==> " << lcs(mat, transmissions[1], transmissions[2]) << endl;
     check << DIVISOR2 << endl;
     
     check.close();
